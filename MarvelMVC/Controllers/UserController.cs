a2bde08225a43673a27e5d3e6a6d8d0c01845909
Nimi77 <abimbolaoladejo4@gmail.com>
2022-08-28 01:29:54 -0700

    Add project files.

---
+++ MarvelMVC/Controllers/UserController.cs
@@ -0,0 +1,33 @@
+﻿using MarvelMVC.Models;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MarvelMVC.Controllers
+{
+    public class UserController : Controller
+    {
+        public static List<UserData> allUsers = new List<UserData>();
+        public IActionResult Index()
+        {
+            return View(allUsers);
+        }
+        public IActionResult AddUser()
+        {
+            return View();
+        }
+        [HttpPost]
+        public IActionResult AddUser(UserData user)
+        {
+            if (ModelState.IsValid)
+            {
+                allUsers.Add(user);
+                return RedirectToAction("AddUser");
+            }
+            return View();
+        }
+
+    }
+}
---
+++ MarvelMVC/Models/UserData.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MarvelMVC.Models
+{
+    public class UserData
+    {
+       [Required(ErrorMessage = "First name is required ")]
+        public string FirstName { get; set; }
+        [Required(ErrorMessage = "Last name is required ")]
+        public string LastName { get; set; }
+        [Required(ErrorMessage = "Location is required ")]
+        public string Location { get; set; }
+        [Required(ErrorMessage = "Phone Number is required ")]
+        public string PhoneNumber { get; set; }
+        [Required(ErrorMessage = "Email is required ")]
+        [EmailAddress(ErrorMessage = "Please enter a valid email address")]
+        public string Email { get; set; }
+
+    }
+}
